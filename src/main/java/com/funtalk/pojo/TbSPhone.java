29e6d400dcf7cc07db4b34023677f5dc7e6f7fa0
Exia-Chris <51144389+Exia-Chris@users.noreply.github.com>
2020-05-15 17:54:37 +0800

    Add files via upload

---
+++ src/main/java/com/funtalk/pojo/TbSPhone.java
@@ -0,0 +1,335 @@
+package com.funtalk.pojo;
+
+import java.util.Date;
+
+public class TbSPhone {
+    private Integer id;
+
+    private String serviceKind;
+
+    private String cityCode;
+
+    private String serviceId;
+
+    private String iccid;
+
+    private String iccid2;
+
+    private String cardId;
+
+    private Integer ifSeed;
+
+    private String seedIccid;
+
+    private String seedcarddata;
+
+    private String data1;
+
+    private String data2;
+
+    private String data3;
+
+    private String data4;
+
+    private String data5;
+
+    private String data6;
+
+    private String data7;
+
+    private String data8;
+
+    private String data9;
+
+    private String data10;
+
+    private String changedata;
+
+    private String deldata;
+
+    private Date createTime;
+
+    private String state;
+
+    private Date stateTime;
+
+    private String phoneChannel;
+
+    private String ifUse;
+
+    private String nowUse;
+
+    private String rssilevel;
+
+    private String delay;
+
+    private String nowIccid;
+
+    private String maochiId;
+
+    private String online;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getServiceKind() {
+        return serviceKind;
+    }
+
+    public void setServiceKind(String serviceKind) {
+        this.serviceKind = serviceKind == null ? null : serviceKind.trim();
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode == null ? null : cityCode.trim();
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId == null ? null : serviceId.trim();
+    }
+
+    public String getIccid() {
+        return iccid;
+    }
+
+    public void setIccid(String iccid) {
+        this.iccid = iccid == null ? null : iccid.trim();
+    }
+
+    public String getIccid2() {
+        return iccid2;
+    }
+
+    public void setIccid2(String iccid2) {
+        this.iccid2 = iccid2 == null ? null : iccid2.trim();
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId == null ? null : cardId.trim();
+    }
+
+    public Integer getIfSeed() {
+        return ifSeed;
+    }
+
+    public void setIfSeed(Integer ifSeed) {
+        this.ifSeed = ifSeed;
+    }
+
+    public String getSeedIccid() {
+        return seedIccid;
+    }
+
+    public void setSeedIccid(String seedIccid) {
+        this.seedIccid = seedIccid == null ? null : seedIccid.trim();
+    }
+
+    public String getSeedcarddata() {
+        return seedcarddata;
+    }
+
+    public void setSeedcarddata(String seedcarddata) {
+        this.seedcarddata = seedcarddata == null ? null : seedcarddata.trim();
+    }
+
+    public String getData1() {
+        return data1;
+    }
+
+    public void setData1(String data1) {
+        this.data1 = data1 == null ? null : data1.trim();
+    }
+
+    public String getData2() {
+        return data2;
+    }
+
+    public void setData2(String data2) {
+        this.data2 = data2 == null ? null : data2.trim();
+    }
+
+    public String getData3() {
+        return data3;
+    }
+
+    public void setData3(String data3) {
+        this.data3 = data3 == null ? null : data3.trim();
+    }
+
+    public String getData4() {
+        return data4;
+    }
+
+    public void setData4(String data4) {
+        this.data4 = data4 == null ? null : data4.trim();
+    }
+
+    public String getData5() {
+        return data5;
+    }
+
+    public void setData5(String data5) {
+        this.data5 = data5 == null ? null : data5.trim();
+    }
+
+    public String getData6() {
+        return data6;
+    }
+
+    public void setData6(String data6) {
+        this.data6 = data6 == null ? null : data6.trim();
+    }
+
+    public String getData7() {
+        return data7;
+    }
+
+    public void setData7(String data7) {
+        this.data7 = data7 == null ? null : data7.trim();
+    }
+
+    public String getData8() {
+        return data8;
+    }
+
+    public void setData8(String data8) {
+        this.data8 = data8 == null ? null : data8.trim();
+    }
+
+    public String getData9() {
+        return data9;
+    }
+
+    public void setData9(String data9) {
+        this.data9 = data9 == null ? null : data9.trim();
+    }
+
+    public String getData10() {
+        return data10;
+    }
+
+    public void setData10(String data10) {
+        this.data10 = data10 == null ? null : data10.trim();
+    }
+
+    public String getChangedata() {
+        return changedata;
+    }
+
+    public void setChangedata(String changedata) {
+        this.changedata = changedata == null ? null : changedata.trim();
+    }
+
+    public String getDeldata() {
+        return deldata;
+    }
+
+    public void setDeldata(String deldata) {
+        this.deldata = deldata == null ? null : deldata.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Date getStateTime() {
+        return stateTime;
+    }
+
+    public void setStateTime(Date stateTime) {
+        this.stateTime = stateTime;
+    }
+
+    public String getPhoneChannel() {
+        return phoneChannel;
+    }
+
+    public void setPhoneChannel(String phoneChannel) {
+        this.phoneChannel = phoneChannel == null ? null : phoneChannel.trim();
+    }
+
+    public String getIfUse() {
+        return ifUse;
+    }
+
+    public void setIfUse(String ifUse) {
+        this.ifUse = ifUse == null ? null : ifUse.trim();
+    }
+
+    public String getNowUse() {
+        return nowUse;
+    }
+
+    public void setNowUse(String nowUse) {
+        this.nowUse = nowUse == null ? null : nowUse.trim();
+    }
+
+    public String getRssilevel() {
+        return rssilevel;
+    }
+
+    public void setRssilevel(String rssilevel) {
+        this.rssilevel = rssilevel == null ? null : rssilevel.trim();
+    }
+
+    public String getDelay() {
+        return delay;
+    }
+
+    public void setDelay(String delay) {
+        this.delay = delay == null ? null : delay.trim();
+    }
+
+    public String getNowIccid() {
+        return nowIccid;
+    }
+
+    public void setNowIccid(String nowIccid) {
+        this.nowIccid = nowIccid == null ? null : nowIccid.trim();
+    }
+
+    public String getMaochiId() {
+        return maochiId;
+    }
+
+    public void setMaochiId(String maochiId) {
+        this.maochiId = maochiId == null ? null : maochiId.trim();
+    }
+
+    public String getOnline() {
+        return online;
+    }
+
+    public void setOnline(String online) {
+        this.online = online == null ? null : online.trim();
+    }
+}
